8a4c52a0e8e768d2994a0515e2a2b58528fc2183
Kaique Sacchi <sacchikaique@gmail.com>
2023-09-18 23:10:11 -0300

    medium: 22, 23, 24

---
+++ medium/23-kebab-case.ts
@@ -0,0 +1,57 @@
+/*
+  612 - KebabCase
+  -------
+  by Johnson Chu (@johnsoncodehk) #medium #template-literal
+
+  ### Question
+
+  Replace the `camelCase` or `PascalCase` string with `kebab-case`.
+
+  `FooBarBaz` -> `foo-bar-baz`
+
+  For example
+
+  ```ts
+  type FooBarBaz = KebabCase<"FooBarBaz">
+  const foobarbaz: FooBarBaz = "foo-bar-baz"
+
+  type DoNothing = KebabCase<"do-nothing">
+  const doNothing: DoNothing = "do-nothing"
+  ```
+
+  > View on GitHub: https://tsch.js.org/612
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type HandleRest<S extends string> = S extends `${infer IFirst}${infer IRest}`
+  ? S extends Uncapitalize<S>
+    ? `${IFirst}${HandleRest<IRest>}`
+    : `-${Uncapitalize<IFirst>}${HandleRest<IRest>}`
+  : S;
+
+type KebabCase<S extends string> = S extends `${infer IFirst}${infer IRest}`
+  ? `${Uncapitalize<IFirst>}${HandleRest<IRest>}`
+  : S;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<KebabCase<"FooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"fooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"foo-bar">, "foo-bar">>,
+  Expect<Equal<KebabCase<"foo_bar">, "foo_bar">>,
+  Expect<Equal<KebabCase<"Foo-Bar">, "foo--bar">>,
+  Expect<Equal<KebabCase<"ABC">, "a-b-c">>,
+  Expect<Equal<KebabCase<"-">, "-">>,
+  Expect<Equal<KebabCase<"">, "">>,
+  Expect<Equal<KebabCase<"😎">, "😎">>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/612/answer
+  > View solutions: https://tsch.js.org/612/solutions
+  > More Challenges: https://tsch.js.org
+*/
---
+++ medium/24-diff.ts
@@ -0,0 +1,52 @@
+/*
+  645 - Diff
+  -------
+  by ZYSzys (@ZYSzys) #medium #object
+
+  ### Question
+
+  Get an `Object` that is the difference between `O` & `O1`
+
+  > View on GitHub: https://tsch.js.org/645
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Prettify<TObj> = {
+  [IKey in keyof TObj]: TObj[IKey];
+};
+
+type Diff<TObj_1, TObj_2> = Prettify<
+  Omit<TObj_1, keyof TObj_2> & Omit<TObj_2, keyof TObj_1>
+>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type Foo = {
+  name: string;
+  age: string;
+};
+type Bar = {
+  name: string;
+  age: string;
+  gender: number;
+};
+type Coo = {
+  name: string;
+  gender: number;
+};
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/645/answer
+  > View solutions: https://tsch.js.org/645/solutions
+  > More Challenges: https://tsch.js.org
+*/
