db421a1209c618340d862c14cd4a5edddefe83c3
Kaique Sacchi <sacchikaique@gmail.com>
2023-10-01 21:30:11 -0300

    summary: added tips

--- summary.ts
+++ summary.ts
@@ -8,6 +8,21 @@ Inferred arguments: Always starting with `I`
 */
 
 /* ---------------------------------------------------------------------------------------------- */
+/*                                            Variables                                           */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+"Variables" can be created by declaring a defaulted generic. It is a way to only calculate its value
+once, and reuse it more cleanly.
+*/
+
+type Variables<
+  T extends object,
+  TKeys extends keyof T = keyof T // Variable
+> = {
+  [IKey in TKeys]: T[IKey];
+};
+
+/* ---------------------------------------------------------------------------------------------- */
 /*                                             Extends                                            */
 /* ---------------------------------------------------------------------------------------------- */
 /*
@@ -15,10 +30,12 @@ If `T extends K`, a value that satisfies `T` must also satisfy `K`
 T is stricter than K, and not the other way around.
 */
 type ExtendsNumber<T> = T extends number ? true : false;
+
 type ZeroExtendsNumber = ExtendsNumber<0>;
 //   ^?
 
 type NumberExtends<T> = number extends T ? true : false;
+
 type NumberExtendsZero = NumberExtends<0>;
 //   ^?
 
@@ -32,6 +49,7 @@ conditional.
 type InferReturnType<T> = T extends (...args: any) => infer IReturn
   ? IReturn
   : never;
+
 type InferredReturnType = InferReturnType<() => 1>;
 //   ^?
 
@@ -58,11 +76,78 @@ You can turn a property optional by adding `?:`, and required by adding `-?:`
 type AllOptional<T extends object> = {
   [Key in keyof T]?: T[Key];
 };
+
 type ExampleAllOptional = AllOptional<{ a: string; b: number }>;
 //   ^?
 
 type AllRequired<T extends object> = {
   [Key in keyof T]-?: T[Key];
 };
+
 type ExampleAllRequired = AllRequired<{ a?: string; b?: number }>;
+//   ^?
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                            To String                                           */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+It is possible to cast a value to string by using template strings
+*/
+
+type ToString<T extends string | number | bigint | boolean | null | undefined> =
+  `${T}`;
+
+type NumberAsString = ToString<20>;
+//   ^?
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                           From String                                          */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+Template strings can also be used to cast to other types
+*/
+
+type ToNumber<T extends string | number | bigint | boolean | null | undefined> =
+  `${T}` extends `${infer ICast extends number}` ? ICast : never;
+
+type StringAsNumber = ToNumber<"1234">;
+//   ^?
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                         Detect `never`                                         */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+It is not possible to directly check if a value `extends never`, as nothing does, not even `never`
+itself. It is possible to cast it to a tuple, and then compare it.
+*/
+
+type IsNever<T> = [T] extends [never] ? true : false;
+
+type OneIsNotNever = IsNever<1>;
+//   ^?
+
+type NeverIsNever = IsNever<never>;
+//   ^?
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                         Number Counter                                         */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+It is not possible to perform increments with numbers. To create a counter, an array can be used
+(using its length). To increment it, just push an element to it. 
+*/
+type IsGreater<
+  TA,
+  TB,
+  TCounter extends true[] = []
+> = TA extends TCounter["length"]
+  ? false
+  : TB extends TCounter["length"]
+  ? true
+  : IsGreater<TA, TB, [...TCounter, true]>;
+
+type ThreeGreaterThanOne = IsGreater<3, 1>;
+//   ^?
+
+type TwoGreaterThanFive = IsGreater<2, 5>;
 //   ^?
