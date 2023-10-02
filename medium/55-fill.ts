016725f35a07ae7fa19277261a7102cf39e4bf7b
Kaique Sacchi <sacchikaique@gmail.com>
2023-10-01 23:22:00 -0300

    medium: 55, 56, 57

---
+++ medium/55-fill.ts
@@ -0,0 +1,75 @@
+/*
+  4518 - Fill
+  -------
+  by キリサメ qianxi (@qianxi0410) #medium #tuple
+
+  ### Question
+
+  `Fill`, a common JavaScript function, now let us implement it with types.
+  `Fill<T, N, Start?, End?>`, as you can see,`Fill` accepts four types of parameters, of which `T` and `N` are required parameters, and `Start` and `End` are optional parameters.
+  The requirements for these parameters are: `T` must be a `tuple`, `N` can be any type of value, `Start` and `End` must be integers greater than or equal to 0.
+
+  ```ts
+  type exp = Fill<[1, 2, 3], 0> // expected to be [0, 0, 0]
+  ```
+  In order to simulate the real function, the test may contain some boundary conditions, I hope you can enjoy it :)
+
+  > View on GitHub: https://tsch.js.org/4518
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Fill<
+  TArray extends unknown[],
+  TFillWith,
+  TStart extends number = 0,
+  TEnd extends number = TArray["length"],
+  TCount extends true[] = [],
+  TState extends "beforeStart" | "afterStart" = "beforeStart"
+> = TArray extends [infer IFirst, ...infer IRest]
+  ? TState extends "beforeStart"
+    ? TCount["length"] extends TStart
+      ? Fill<TArray, TFillWith, TStart, TEnd, TCount, "afterStart"> // Starts replacing
+      : // Skips before start
+        [
+          IFirst,
+          ...Fill<
+            IRest,
+            TFillWith,
+            TStart,
+            TEnd,
+            [...TCount, true],
+            "beforeStart"
+          >
+        ]
+    : TCount["length"] extends TEnd
+    ? TArray
+    : [
+        TFillWith,
+        ...Fill<IRest, TFillWith, TStart, TEnd, [...TCount, true], "afterStart">
+      ]
+  : [];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Fill<[], 0>, []>>,
+  Expect<Equal<Fill<[], 0, 0, 3>, []>>,
+  Expect<Equal<Fill<[1, 2, 3], 0, 0, 0>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], 0, 2, 2>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], 0>, [0, 0, 0]>>,
+  Expect<Equal<Fill<[1, 2, 3], true>, [true, true, true]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 0, 1>, [true, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 1, 3>, [1, true, true]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 10, 0>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 10, 20>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 0, 10>, [true, true, true]>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4518/answer
+  > View solutions: https://tsch.js.org/4518/solutions
+  > More Challenges: https://tsch.js.org
+*/
