15020f4cce5dab3f525c58a951b0d3b193fd88b9
Kaique Sacchi <sacchikaique@gmail.com>
2023-10-18 22:46:19 -0300

    medium: 85, 86, 87

---
+++ medium/81-100/86-triangular-number.ts
@@ -0,0 +1,42 @@
+/*
+  27152 - Triangular number
+  -------
+  by null (@aswinsvijay) #medium #tuple #array #math
+
+  ### Question
+
+  Given a number N, find the Nth triangular number, i.e. `1 + 2 + 3 + ... + N`
+
+  > View on GitHub: https://tsch.js.org/27152
+*/
+
+/* _____________ Your Code Here _____________ */
+import type { Sum } from "../../utils/sum";
+
+type Triangular<
+  N extends number,
+  TCount extends number = 0,
+  TResult extends number = 0
+> = TCount extends N
+  ? Sum<TCount, TResult>
+  : Triangular<N, Sum<TCount, 1>, Sum<TResult, TCount>>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Triangular<0>, 0>>,
+  Expect<Equal<Triangular<1>, 1>>,
+  Expect<Equal<Triangular<3>, 6>>,
+  Expect<Equal<Triangular<10>, 55>>,
+  Expect<Equal<Triangular<20>, 210>>,
+  Expect<Equal<Triangular<55>, 1540>>,
+  Expect<Equal<Triangular<100>, 5050>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/27152/answer
+  > View solutions: https://tsch.js.org/27152/solutions
+  > More Challenges: https://tsch.js.org
+*/
